274ddc1ecc248aec9b1112214c6c0539aa46ac0b
Behzad <bsadrfa@g.clemson.edu>
2020-12-18 01:30:43 -0500

    update filepath

--- comsol/matlab/batch_biotac.m
+++ comsol/matlab/batch_biotac.m
@@ -1,32 +1,32 @@
 savefolder = "/home/bsadrfa/behzad/projects/biotac/comsol/results/";
 cfg_folder = savefolder + "configs/";
-cfg_base = cfg_folder + "base.csv";
-t = readtable(cfg_base);
-parameters = t.(1);
-values = t.(2);
-for i=1:size(values,1)
-    model.param.set(parameters(i), values(i));
-end
-
-model.component('comp1').geom('geom1').run;
-model.component('comp1').geom('geom1').run('fin');
-model.component('comp1').mesh('mesh2').run;
-model.component('comp1').mesh('mesh3').run;
-model.component('comp1').mesh('mesh4').run;
-
-model.study('std1').feature('stat').set('pname', {'para'});
-model.study('std1').feature('stat').set('plistarr', {'range(0,istep,istop)'});
-model.study('std1').feature('stat').set('punit', {''});
-
-model.sol('sol1').feature('v1').set('clistctrl', {'p1'});
-model.sol('sol1').feature('v1').set('cname', {'para'});
-model.sol('sol1').feature('v1').set('clist', {'range(0,istep,istop)'});
-
-model.sol('sol1').feature('s1').feature('p1').set('pname', {'para'});
-model.sol('sol1').feature('s1').feature('p1').set('plistarr', {'range(0,istep,istop)'});
-model.sol('sol1').feature('s1').feature('p1').set('punit', {''});
-
-model.study('std1').run
+% cfg_base = cfg_folder + "base.csv";
+% t = readtable(cfg_base);
+% parameters = t.(1);
+% values = t.(2);
+% for i=1:size(values,1)
+%     model.param.set(parameters(i), values(i));
+% end
+% 
+% model.component('comp1').geom('geom1').run;
+% model.component('comp1').geom('geom1').run('fin');
+% model.component('comp1').mesh('mesh2').run;
+% model.component('comp1').mesh('mesh3').run;
+% model.component('comp1').mesh('mesh4').run;
+% 
+% model.study('std1').feature('stat').set('pname', {'para'});
+% model.study('std1').feature('stat').set('plistarr', {'range(0,istep,istop)'});
+% model.study('std1').feature('stat').set('punit', {''});
+% 
+% model.sol('sol1').feature('v1').set('clistctrl', {'p1'});
+% model.sol('sol1').feature('v1').set('cname', {'para'});
+% model.sol('sol1').feature('v1').set('clist', {'range(0,istep,istop)'});
+% 
+% model.sol('sol1').feature('s1').feature('p1').set('pname', {'para'});
+% model.sol('sol1').feature('s1').feature('p1').set('plistarr', {'range(0,istep,istop)'});
+% model.sol('sol1').feature('s1').feature('p1').set('punit', {''});
+% 
+% model.study('std1').run
 
 fid = 0;
 for i=1:1
@@ -35,7 +35,9 @@ for i=1:1
     import com.comsol.model.util.*
     % biotac_model_.mph has "finer" mesh preset
     % model = mphopen('/home/bsadrfa/behzad/projects/shadowhand/comsol/biotac_model.mph');%
-    model = mphopen('/home/bsadrfa/behzad/projects/biotac/comsol/model/biotac.mph');
+%     model = mphopen('/home/bsadrfa/behzad/projects/biotac/comsol/model/biotac.mph');
+    model = mphopen('/home/bsadrfa/behzad/projects/biotac/comsol/model/biotac-thick_shore_a_35');
+    
     ModelUtil.showProgress(true);
 end
 
