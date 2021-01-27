6d6851e960f1e6b1071da8818ad3ae1919f30347
Behzad <bsadrfa@g.clemson.edu>
2021-01-26 22:44:50 -0500

    added results for theta_10 and theta_20

--- comsol/matlab/sim_biotac.m
+++ comsol/matlab/sim_biotac.m
@@ -4,19 +4,24 @@ savefolder = "/home/bsadrfa/behzad/projects/biotac/comsol/results/";
 % load comsol file
 import com.comsol.model.*
 import com.comsol.model.util.*
-model = mphopen('/home/bsadrfa/behzad/projects/biotac/comsol/model/biotac.mph');
+% model = mphopen('/home/bsadrfa/behzad/projects/biotac/comsol/model/biotac.mph');
+model = mphopen('/home/bsadrfa/behzad/projects/biotac/comsol/model/data/biotac_theta_20.mph');
 ModelUtil.showProgress(true);
 
 % meshgrid of rays in u,v coordinates
-n = 20;
+n = 50;
 offset = 0.05*pi;
 rays_uv = Ray.uvGrid(n, offset);
-rays_xyz = Ray.skinGrid(n, offset);
+rays_xyz = Ray.skinGrid(n, offset);model = mphopen('/home/bsadrfa/behzad/projects/biotac/comsol/model/biotac.mph');
+
 
 % Create a Biotac instance
-biotac = Biotac(model, 1, savefolder);
+biotac = Biotac(model, 3, savefolder);
 biotac.setSkins;
 
 % biotac.spin
-% biotac.saveData;
-biotac.plotSkin;
+biotac.saveData;
+% biotac.plotSkin;
+
+writematrix(rays_xyz, sprintf('%s/rays_xyz.csv', biotac.SaveFolder))
+writematrix(rays_uv, sprintf('%s/rays_uv.csv', biotac.SaveFolder))
